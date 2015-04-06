57a3660bcbf5b2413b4031a3abb32f077c8aef22
Chinmay Gadgil <chinmay.gadgil@P29564.intra.rakuten.co.jp>
2015-04-06 10:46:07 +0900

    Re-usable Pointcut

--- src/jp/co/rakuten/checkout/sap/Logger.java
+++ src/jp/co/rakuten/checkout/sap/Logger.java
@@ -2,14 +2,19 @@ package jp.co.rakuten.checkout.sap;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class Logger {
+      
+      @Pointcut("execution(void jp.co.rakuten.checkout.sap.Camera.takeSnap())")
+      public void cameraSnap() {
+          
+      }
     
-    
-      @Before("execution(void jp.co.rakuten.checkout.sap.Camera.takeSnap())")
+      @Before("cameraSnap()")
       public void aboutToSnap() {
     	  System.out.println("About to Snap");
       }

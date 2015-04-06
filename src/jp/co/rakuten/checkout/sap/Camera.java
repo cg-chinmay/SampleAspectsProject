3254628c1dddd3a97d599eafcb40bff126b71a83
Chinmay Gadgil <chinmay.gadgil@P29564.intra.rakuten.co.jp>
2015-04-06 10:43:44 +0900

    PointCut with Annotations

--- src/jp/co/rakuten/checkout/sap/Camera.java
+++ src/jp/co/rakuten/checkout/sap/Camera.java
@@ -1,5 +1,8 @@
 package jp.co.rakuten.checkout.sap;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Camera {
    public void takeSnap() {
 	   System.out.println("Take Snap");
--- src/jp/co/rakuten/checkout/sap/Logger.java
+++ src/jp/co/rakuten/checkout/sap/Logger.java
@@ -1,6 +1,15 @@
 package jp.co.rakuten.checkout.sap;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
 public class Logger {
+    
+    
+      @Before("execution(void jp.co.rakuten.checkout.sap.Camera.takeSnap())")
       public void aboutToSnap() {
     	  System.out.println("About to Snap");
       }
